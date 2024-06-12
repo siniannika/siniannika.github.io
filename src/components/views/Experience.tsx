35a69ee2506613db3fb3420e2ad4e6493563ee04
rrobben <roope.poyry1@gmail.com>
2024-06-12 11:57:10 +0300

    Updated timeline

--- src/components/views/Experience.tsx
+++ src/components/views/Experience.tsx
@@ -3,7 +3,7 @@ import Layout from "../layout/Layout";
 const Experience = () => {
     return (
         <Layout>
-            <img src="Työhistorian aikajana.jpg" alt="Aikajana" width="100%" />
+            <img src="Työhistorian aikajana.png" alt="Aikajana" width="100%" />
         </Layout>
     );
 };
